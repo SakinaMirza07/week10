5a8ad95b16e4310b0027cea73c6e5b7d15bf54af
Sakina Mirza <sakina34540@gmail.com>
2024-11-26 15:42:46 +0400

    Adding all files

---
+++ products.js
@@ -0,0 +1,47 @@
+let products = [
+    {
+        id: 1001,
+        title: "Cat Food, 25lb bag",
+        description: "A 25 pound bag of <em>irresistible</em>," + "organic goodness for your cat.",
+        price: 20.00,
+        image: "kitty.webp",
+        availableInventory: 10,
+        rating: 3
+    },
+    {
+        id: 1002,
+        title: "Yarn",
+        description: "Your cat can play with for a very <strong>long</strong> time.",
+        price: 9.89,
+        image: "kitty.webp",
+        availableInventory: 10,
+        rating: 3   
+    },
+    {
+        id: 1003,
+        title: "Ball",
+        description: "Your cat can play with for a very <strong>long</strong> time.",
+        price: 5.99,
+        image: "kitty.webp",
+        availableInventory: 10,
+        rating: 4
+    },
+    {
+        id: 1004,
+        title: "Pillow",
+        description: "For your cat to sleep comfortably.",
+        price: 16.99,
+        image: "kitty.webp",
+        availableInventory: 10,
+        rating: 5
+    },
+    {
+        id: 1005,
+        title: "Litter Box",
+        description: " Indoor toilet, filled with absorbent material like litter to manage waste.",
+        price: 19.00,
+        image: "kitty.webp",
+        availableInventory: 10,
+        rating: 3
+    }
+]
